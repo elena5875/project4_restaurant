f245c8e59a8443d79099f19faad92864b5356821
Elena freire <elenafreire75@gmail.com>
2024-02-25 21:22:53 +0000

    adding code for deployment in heroku

--- project4/settings.py
+++ project4/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k3y!#_g1xtxgnwz)k+-)))4+r6=^3t-xyk7)9j+0l&=l=1b_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-elena5875-project4resta-k57v6av2xsw.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-elena5875-project4resta-k57v6av2xsw.ws-eu108.gitpod.io',
+    '.herokuapp.com']
 
 
 # Application definition
