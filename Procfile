77ab4bff5a9228a212023493570980138afb1076
elena5875 <elenafreire75@gmail.com>
2024-03-04 09:39:17 +0100

    repository from github

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn project4_restaurant.wsgi --log-file -
-
--- static/js/contact-info.js
+++
@@ -1,8 +0,0 @@
-// contact-info.js
-
-// This file contains JavaScript code related to the contact information section
-
-// Function to display contact information
-function displayContactInfo() {
-    alert("For inquiries please contact us through the following:\nEmail: theforkrestaurant@gmail.com\nTel. Number: 467925123\nAddress: Brunbärsvagen 7, 17754 Jarfalla, Stockholm City");
-}
